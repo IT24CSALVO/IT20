0a9dfa9c9882440ee8d784f0c42352190cfeeed2
IT24CSALVO <alliahshanesalvo11@gmail.com>
2024-10-03 01:10:03 +0800

    linkedlist

---
+++ Topic_2_Linkedlist_int/Node.java
@@ -0,0 +1,13 @@
+package Topic_2_Linkedlist_int;
+
+public class Node {
+    int data;  
+    Node next;  
+
+    // Constructor
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
