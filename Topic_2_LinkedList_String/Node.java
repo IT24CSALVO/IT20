647305f0559c967e4b7234023b9da11c1b32885c
IT24CSALVO <alliahshanesalvo11@gmail.com>
2024-10-04 03:46:08 +0800

    linkedlist string

---
+++ Topic_2_LinkedList_String/Node.java
@@ -0,0 +1,14 @@
+package Topic_2_LinkedList_String;
+
+public class Node {
+    String data;  
+    Node next;    
+
+    // Constructor
+ Node(String data) {
+        this.data = data;
+        this.next = null;
+    }
+
+}
+
